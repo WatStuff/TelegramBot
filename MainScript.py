3a7b8b99a5a08a60f0ca4533e8850b12c3585a18
KpStuffOrigins <43894864+KpStuffOrigins@users.noreply.github.com>
2018-10-07 00:45:14 +0200

    Update MainScript.py

--- MainScript.py
+++ MainScript.py
@@ -2,27 +2,52 @@ import json
 import requests
 import time
 
-token="603809094:AAGSs4gbLqhK8xfqicOwAIDWN3eTwXfuy6k"
-URL = "https://api.telegram.org/bot{}/".format(token)
+TOKEN = "603809094:AAGSs4gbLqhK8xfqicOwAIDWN3eTwXfuy6k"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
 
 def get_url(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
     return content
 
+
 def get_json_from_url(url):
     content = get_url(url)
     js = json.loads(content)
     return js
+
 
 def get_updates():
     url = URL + "getUpdates"
     js = get_json_from_url(url)
     return js
 
-while True:
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
     text = updates["result"][last_update]["message"]["text"]
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return (text, chat_id)
+
+
+def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
-    time.sleep(0.5)
+
+
+text, chat = get_last_chat_id_and_text(get_updates())
+send_message(text, chat)
+
+def main():
+    last_textchat = (None, None)
+    while True:
+        text, chat = get_last_chat_id_and_text(get_updates())
+        if (text, chat) != last_textchat:
+            send_message(text, chat)
+            last_textchat = (text, chat)
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
